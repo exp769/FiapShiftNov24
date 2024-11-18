2e546810499ba1f40124b3a7b616191845588c81
exp769 <leotokuo@gmail.com>
2024-11-18 20:12:35 -0300

    void

---
+++ FASTAPI/app/main.py
@@ -0,0 +1,12 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/livro/livro_id")
+def read_livro(livro_id: int, q:Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
